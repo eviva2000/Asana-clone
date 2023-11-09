96879a840794fc68822d0cfee8e0e2ceab0b95bd
eviva2000 <arjomandielaheh@gamin.com>
2023-11-09 22:05:51 +0100

    added editor config

--- backend/src/config/db-config.ts
+++ backend/src/config/db-config.ts
@@ -2,7 +2,6 @@ import { config } from '../knexfile';
 import knex from 'knex';
 const currentEnv: string = process.env.ENVIRONMENT || 'development';
 const db = knex(config[currentEnv]);
-console.log('b');
 db.raw('SELECT VERSION()')
   .then(() => {
     console.warn(`connection to db successful!`);
